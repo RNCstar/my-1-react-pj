d4ee4c6a8786e63e6a8dd5d700510149a4465bb8
RNCstar <r.n.c.star2002@gmail.com>
2024-05-17 03:11:54 -0700

    finish design

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { ImEyeBlocked } from "react-icons/im";
+import datas from "./data";
 import { useState } from "react";
+import data from "./data";
 function App() {
-  const [type, setType] = useState("password");
-  const [icon, setIcon] = useState("none");
-  const handleClick = () => {
-    if (type === "password") {
-      setType("text");
-    } else {
-      setType("password");
-    }
-    if (icon === "none") {
-      setIcon("block");
-    } else {
-      setIcon("none");
-    }
-  };
   return (
-    <div className="App">
-      <div className="wrapper">
-        <div className="input-fields">
-          <span onClick={handleClick}>
-            <ImEyeBlocked style={{ fontSize: "30px" }} />
-          </span>
-          <input type={type} />
-        </div>
-      </div>
+    <div className="App section-center">
+      <h4>لورم ساز ما </h4>
+      <form className="lorem">
+        <lable for="loremNO" id="LoremNO">
+          تعداد پاراگراف
+        </lable>
+        <input type="number" />
+        <button type="button" className="makeIt">
+          بساز
+        </button>
+      </form>
+      <article className="lorem-text-box">
+        <p className="lorem-text">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
+          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
+          در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+          طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+          الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
+          صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
+          شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
+          اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
+          قرار گیرد.
+        </p>
+      </article>
     </div>
   );
 }
